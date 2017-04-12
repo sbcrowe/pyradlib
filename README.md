8cdac329e4d24510c9a88f67f06b148d022becc3
sbcrowe <sb.crowe@gmail.com>
2017-04-12 14:28:56 +1000

    Add film calibration functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Python Radiotherapy Library
 A suite of software tools developed for Radiation Oncology Medical Physics research work. Contains Python implementation of functionality provided by earlier MCDTK, TADA, DICOM CT Expander, DICOM Anonymiser and DICOM Sorter software developed by Scott Crowe.
+
+## License
+See LICENSE.md for license information.
---
+++ radlib/film/calibration.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+""" Gafchromic film calibration module.
+
+This module provides dose calibration functionality for radiochromic film.
+"""
+
+# authorship information
+__author__ = 'Scott Crowe'
+__email__ = 'sb.crowe@gmail.com'
+__credits__ = ['Tanya Kairn']
+__license__ = "GPL3"
+
+# import required code
+import numpy as np
+from scipy import optimize
+from numpy.polynomial.polynomial import polyfit
+from numpy.polynomial.polynomial import polyval
+
+
+class NonLinearFit:
+    """Performs calibration fit using Devic non-linear fitting function."""
+
+    name = 'Non-linear fit'
+    variable = 'netOD'
+
+    def __init__(self, dose_data, netod_data):
+        """Instantiates the calibration fit using a non-linear function.
+
+        Args:
+            dose_data (np.array): The calibration doses delivered.
+            netod_data (np.array): The net optical densities of irradiated film.
+        """
+        # assert pre-conditions
+        assert len(dose_data) == len(netod_data), 'Number of dose and netOD data points disagree.'
+        assert len(dose_data) > 4, 'Insufficient number of data points for non-linear fit.'
+        # instantiate class variables
+        self.dose_data = dose_data
+        self.netod_data = netod_data
+        self.minimum = np.min(netod_data)
+        self.maximum = np.max(netod_data)
+        popt, pcov = optimize.curve_fit(self._non_linear_fitting_function,
+                                        netod_data, dose_data, p0=[1, 0, 0, 2])
+        self.a, self.b, self.c, self.n = popt
+        self.degrees_of_freedom = len(dose_data) - 4
+        calculated_dose = self.dose(np.array(netod_data))
+        self.rmse = np.sqrt(np.sum((np.array(dose_data) - calculated_dose) ** 2) / self.degrees_of_freedom)
+
+    def _non_linear_fitting_function(x, a, b, c, n):
+        """The Devic non-linear fitting function."""
+        return a * x ** n + b * x + c
+
+    def dose(self, netod):
+        """Returns dose calculated for provided netOD."""
+        return self._non_linear_fitting_function(netod, self.a, self.b, self.c, self.n)
+
+
+class PolynomialFit:
+    """Performs calibration fit using polynomial fitting function."""
+
+    name = 'Polynomial fit'
+    variable = 'netOD'
+
+    def __init__(self, dose_data, netod_data, order):
+        """Instantiates the calibration fit using a polynomial."""
+        # assert pre-conditions
+        assert len(dose_data) == len(netod_data), 'Number of dose and netOD data points disagree.'
+        assert len(dose_data) > (order + 1), 'Insufficient number of data points for polynomial fit.'
+        # instantiate class variables
+        self.dose_data = dose_data
+        self.netod_data = netod_data
+        self.minimum = np.min(netod_data)
+        self.maximum = np.max(netod_data)
+        self.order = order
+        self.coefficients = polyfit(netod_data, dose_data, order)
+        self.degrees_of_freedom = len(dose_data) - (order + 1)
+        calculated_dose = self.dose(np.array(netod_data))
+        self.rmse = np.sqrt(np.sum((np.array(dose_data) - calculated_dose) ** 2) / self.degrees_of_freedom)
+
+    def dose(self, netod):
+        """Returns dose corresponding to provided netOD."""
+        return polyval(netod, self.coefficients)
+
+
+class TamponiFit:
+    """Performs calibration fit using Tamponi fitting function."""
+
+    name = 'Tamponi fit'
+    variable = 'netOD'
+
+    def __init__(self, dose_data, netod_data):
+        """Instantiates the calibration fit using the Tamponi method."""
+        # assert pre-conditions
+        assert len(dose_data) == len(netod_data), 'Number of dose and netOD data points disagree.'
+        assert len(dose_data) > 1, 'Insufficient number of data points for Tamponi fit.'
+        # instantiate class variables
+        self.dose_data = dose_data
+        self.netod_data = netod_data
+        self.minimum = np.min(netod_data)
+        self.maximum = np.max(netod_data)
+        self.ref_dose = dose_data[-1]
+        self.ref_netod = netod_data[-1]
+        R = np.array(dose_data) / self.ref_dose
+        eR = np.array(netod_data) / self.ref_netod
+        popt, pcov = optimize.curve_fit(self._tamponi_fitting_function, eR, R, p0=[0.5])
+        self.a = popt[0]
+        calculated_dose = self.dose(np.array(netod_data))
+        self.degrees_of_freedom = len(dose_data) - 1
+        self.rmse = np.sqrt(np.sum((np.array(dose_data) - calculated_dose) ** 2) / self.degrees_of_freedom)
+
+    def _tamponi_fitting_function(R, a):
+        """The Tamponi fitting function."""
+        return np.log10(1 + (a - 1) * R) / np.log10(a)
+
+    def dose(self, netod):
+        """Returns dose corresponding to provided netOD."""
+        eR = netod / self.ref_netod
+        # return self.ref_dose * np.log(1+(self.a-1)*eR) / np.log(self.a)
+        return self._tamponi_fitting_function(eR, self.a)
+
+
+class LewisFit:
+    """Performs calibration fit using Lewis fitting function."""
+
+    name = 'Lewis fit'
+    variable = 'Net response'
+
+    def __init__(self, dose_data, response_data):
+        """Instantiates the calibration fit using the Tamponi method."""
+        # assert pre-conditions
+        assert len(dose_data) == len(response_data), 'Number of dose and net response data points disagree.'
+        assert len(dose_data) > 3, 'Insufficient number of data points for Lewis fit.'
+        # instantiate class variables
+        self.dose_data = dose_data
+        self.response_data = response_data
+        self.minimum = np.min(response_data)
+        self.maximum = np.max(response_data)
+        popt, pcov = optimize.curve_fit(self._lewis_fitting_function, response_data, dose_data, p0=[0, 1, 0])
+        self.a, self.b, self.c = popt
+        calculated_dose = self.dose(np.array(response_data))
+        self.degrees_of_freedom = len(dose_data) - 3
+        self.rmse = np.sqrt(np.sum((np.array(dose_data) - calculated_dose) ** 2) / self.degrees_of_freedom)
+
+    def _lewis_fitting_function(x, a, b, c):
+        """The Lewis fitting function."""
+        return ((b) / (x + a)) + c
+
+    def dose(self, response):
+        """Returns dose corresponding to provided net response."""
+        return self._lewis_fitting_function(response, self.a, self.b, self.c)
+
+
+class YaoFit:
+    """Performs calibration fit using Yao fitting function.
+
+    Note: the equation presented by Yao: PR = (1+hD/m) / (1+D/m) can be
+    inverted as D = m(PR-1) / (h-PR); where PR = PV / PV_0
+    """
+
+    name = 'Yao fit'
+    variable = 'Net response'
+
+    def __init__(self, dose_data, response_data):
+        """Instantiates the calibration fit using the Yao method."""
+        # assert pre-conditions
+        assert len(dose_data) == len(response_data), 'Number of dose and net response data points disagree.'
+        assert len(dose_data) > 2, 'Insufficient number of data points for Yao fit.'
+        # instantiate class variables
+        self.dose_data = dose_data
+        self.response_data = response_data
+        self.minimum = np.min(response_data)
+        self.maximum = np.max(response_data)
+        median_dose = np.median(dose_data)
+        popt, pcov = optimize.curve_fit(self._yao_fitting_function, response_data, dose_data, p0=[0.19, median_dose])
+        self.h, self.m = popt
+        calculated_dose = self.dose(np.array(response_data))
+        self.degrees_of_freedom = len(dose_data) - 2
+        self.rmse = np.sqrt(np.sum((np.array(dose_data) - calculated_dose) ** 2) / self.degrees_of_freedom)
+
+    def _yao_fitting_function(x, a, b):
+        """The Yao fitting function."""
+        return (b * (x - 1) / (a - x))
+
+    def dose(self, response):
+        """Returns dose corresponding to provided response ratio."""
+        return self._yao_fitting_function(response, self.h, self.m)
